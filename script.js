11e78cb5bf715b75353d0a23131c01b495f84d8e
kassianoferreira-hue <kassiano.ferreira@escola.pr.gov.br>
2026-06-09 11:18:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Função criada para mostrar uma mensagem
+
+function mostrarMensagem(){
+
+
+    // Exibe uma janela com mensagem
+
+        alert("Parabéns! Você criou uma interação com JavaScript.");
+
+
+        }
+
+    
